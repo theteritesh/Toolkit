a1505a1f14659e80fca27452c58d1d24fb1800ee
theteritesh <theteritesh2@gmail.com>
2024-02-24 19:29:17 +0530

    changes in dashboard add other logos to button

--- Dashboard.py
+++ Dashboard.py
@@ -64,7 +64,7 @@ class dash1:
         btn_Wheather=Button(second_frame,text='Weather',image=wheather_logo,font=('Georgia',14),bg="#A3842C",fg='black',
                             cursor='hand2',compound="top", height=110, width=230,command=self.calculator)
         btn_Wheather.image = wheather_logo 
-        btn_Wheather.grid(row=2,column=0,padx=10,pady=10)
+        btn_Wheather.grid(row=1,column=1,padx=10,pady=10)
 
         notepad_logo = PhotoImage(file="img/notepad_logo.png")
         width = 70
@@ -73,7 +73,7 @@ class dash1:
         btn_notepad=Button(second_frame,text='Notepad',image=notepad_logo,font=('Georgia',14),bg="#A3842C",fg='black',
                             cursor='hand2',compound="top", height=110, width=230)
         btn_notepad.image = notepad_logo 
-        btn_notepad.grid(row=3,column=0,padx=10,pady=10)
+        btn_notepad.grid(row=1,column=2,padx=10,pady=10)
 
         pdf_logo = PhotoImage(file="img/pdf_logo.png")
         width = 70
@@ -82,7 +82,7 @@ class dash1:
         btn_pdf_viewer=Button(second_frame,text='PDF Viewer',image=pdf_logo,font=('Georgia',14),bg="#A3842C",fg='black',
                             cursor='hand2',compound="top", height=110, width=230)
         btn_pdf_viewer.image = pdf_logo
-        btn_pdf_viewer.grid(row=4,column=0,padx=10,pady=10)
+        btn_pdf_viewer.grid(row=1,column=3,padx=10,pady=10)
 
         qr_logo = PhotoImage(file="img/qr_logo.png")
         width = 70
@@ -91,7 +91,7 @@ class dash1:
         btn_qr_gen=Button(second_frame,text='QR Generator',image=qr_logo,font=('Georgia',14),bg="#A3842C",fg='black',
                             cursor='hand2',compound="top", height=110, width=230)
         btn_qr_gen.image = qr_logo
-        btn_qr_gen.grid(row=5,column=0,padx=10,pady=10)
+        btn_qr_gen.grid(row=2,column=0,padx=10,pady=10)
 
         clock_logo = PhotoImage(file="img/clock_logo.png")
         width = 70
@@ -100,46 +100,97 @@ class dash1:
         btn_clock=Button(second_frame,image=clock_logo,text='Clock',font=('Georgia',14),bg="#A3842C",fg='black',
                             cursor='hand2', compound="top",height=110,width=230)
         btn_clock.image = clock_logo
-        btn_clock.grid(row=1,column=1,padx=10,pady=10)
+        btn_clock.grid(row=2,column=1,padx=10,pady=10)
         
-        btn_speed_check=Button(second_frame,text='Speed Check',font=('Georgia',14),bg="#A3842C",fg='black',
-                            cursor='hand2',height=3,width=15)
-        btn_speed_check.grid(row=2,column=1,padx=10,pady=10)
-
-        btn_img_comp=Button(second_frame,text='IMG Comp',font=('Georgia',14),bg="#A3842C",fg='black',
-                            cursor='hand2',height=3,width=15)
-        btn_img_comp.grid(row=3,column=1,padx=10,pady=10)
-
-        btn_to_do=Button(second_frame,text='TO DO',font=('Georgia',14),bg="#A3842C",fg='black',
-                            cursor='hand2',height=3,width=15)
-        btn_to_do.grid(row=4,column=1,padx=10,pady=10)
-
-        btn_white_bord=Button(second_frame,text='White Bord',font=('Georgia',14),bg="#A3842C",fg='black',
-                            cursor='hand2',height=3,width=15)
-        btn_white_bord.grid(row=5,column=1,padx=10,pady=10)
+        speed_test_logo = PhotoImage(file="img/speed_test_logo.png")
+        width = 70
+        height = 70
+        speed_test_logo = speed_test_logo.subsample(speed_test_logo.width() // width, speed_test_logo.height() // height)
+        btn_speed_test=Button(second_frame,image=speed_test_logo,text='Speed Test',font=('Georgia',14),bg="#A3842C",fg='black',
+                            cursor='hand2',height=110,width=230,compound="top")
+        btn_speed_test.image = speed_test_logo
+        btn_speed_test.grid(row=2,column=2,padx=10,pady=10)
         
-        btn_calender=Button(second_frame,text='Calender',font=('Georgia',14),bg="#A3842C",fg='black',
-                            cursor='hand2',height=3,width=15)
-        btn_calender.grid(row=1,column=2,padx=10,pady=10)
+        img_comp_logo = PhotoImage(file="img/img_comp_logo.png")
+        width = 70
+        height = 70
+        img_comp_logo = img_comp_logo.subsample(img_comp_logo.width() // width, img_comp_logo.height() // height)
+        btn_img_comp=Button(second_frame,image=img_comp_logo,text='IMG Comp',font=('Georgia',14),bg="#A3842C",fg='black',
+                            cursor='hand2',height=110,width=230,compound="top")
+        btn_img_comp.image = img_comp_logo
+        btn_img_comp.grid(row=2,column=3,padx=10,pady=10)
 
-        btn_translator=Button(second_frame,text='Translator',font=('Georgia',14),bg="#A3842C",fg='black',
-                            cursor='hand2',height=3,width=15)
-        btn_translator.grid(row=2,column=2,padx=10,pady=10)
+        to_do_logo = PhotoImage(file="img/to_do_logo.png")
+        width = 70
+        height = 70
+        to_do_logo = to_do_logo.subsample(to_do_logo.width() // width, to_do_logo.height() // height)
+        btn_to_do=Button(second_frame,text='TO DO',image=to_do_logo,font=('Georgia',14),bg="#A3842C",fg='black',
+                            cursor='hand2',height=110,width=230,compound="top")
+        btn_to_do.image = to_do_logo
+        btn_to_do.grid(row=3,column=0,padx=10,pady=10)
 
-        btn_demo1=Button(second_frame,text='Demo1',font=('Georgia',14),bg="#A3842C",fg='black',
-                            cursor='hand2',height=3,width=15)
-        btn_demo1.grid(row=3,column=2,padx=10,pady=10)
+        white_board_logo = PhotoImage(file="img/white_board_logo.png")
+        width = 70
+        height = 70
+        white_board_logo = white_board_logo.subsample(white_board_logo.width() // width, white_board_logo.height() // height)
+        btn_white_bord=Button(second_frame,image=white_board_logo,text='White Bord',font=('Georgia',14),bg="#A3842C",fg='black',
+                            cursor='hand2',height=110,width=230,compound="top")
+        btn_white_bord.image = white_board_logo
+        btn_white_bord.grid(row=3,column=1,padx=10,pady=10)
+        
+        calender_logo = PhotoImage(file="img/calendar_logo.png")
+        width = 70
+        height = 70
+        calender_logo = calender_logo.subsample(calender_logo.width() // width, calender_logo.height() // height)
+        btn_calender=Button(second_frame,image=calender_logo,text='Calender',font=('Georgia',14),bg="#A3842C",fg='black',
+                            cursor='hand2',height=110,width=230,compound="top")
+        btn_calender.image = calender_logo
+        btn_calender.grid(row=3,column=2,padx=10,pady=10)
 
-        btn_demo2=Button(second_frame,text='demo2',font=('Georgia',14),bg="#A3842C",fg='black',
-                            cursor='hand2',height=3,width=15)
-        btn_demo2.grid(row=4,column=2,padx=10,pady=10)
-        btn_demo3=Button(second_frame,text='demo3',font=('Georgia',14),bg="#A3842C",fg='black',
-                            cursor='hand2',height=3,width=15)
-        btn_demo3.grid(row=5,column=2,padx=10,pady=10)
+        translator_logo = PhotoImage(file="img/translator_logo.png")
+        width = 70
+        height = 70
+        translator_logo = translator_logo.subsample(translator_logo.width() // width, translator_logo.height() // height)
+        btn_translator=Button(second_frame,image=translator_logo,text='Translator',font=('Georgia',14),bg="#A3842C",fg='black',
+                            cursor='hand2',height=110,width=230,compound="top")
+        btn_translator.image = translator_logo
+        btn_translator.grid(row=3,column=3,padx=10,pady=10)
 
-        btn_demo4=Button(second_frame,text='Translator',font=('Georgia',14),bg="#A3842C",fg='black',
-                            cursor='hand2',height=3,width=15)
-        btn_demo4.grid(row=1,column=3,padx=10,pady=10)
+        demo_logo1 = PhotoImage(file="img/demo_logo.png")
+        width = 70
+        height = 70
+        demo_logo1 = demo_logo1.subsample(demo_logo1.width() // width, demo_logo1.height() // height)
+        btn_demo1=Button(second_frame,image=demo_logo1,text='Demo1',font=('Georgia',14),bg="#A3842C",fg='black',
+                            cursor='hand2',height=110,width=230,compound="top")
+        btn_demo1.image = demo_logo1
+        btn_demo1.grid(row=4,column=0,padx=10,pady=10)
+
+        demo_logo2 = PhotoImage(file="img/demo_logo.png")
+        width = 70
+        height = 70
+        demo_logo2 = demo_logo2.subsample(demo_logo2.width() // width, demo_logo2.height() // height)
+        btn_demo2=Button(second_frame,image=demo_logo2,text='demo2',font=('Georgia',14),bg="#A3842C",fg='black',
+                            cursor='hand2',height=110,width=230,compound="top")
+        btn_demo2.image = demo_logo2
+        btn_demo2.grid(row=4,column=1,padx=10,pady=10)
+        
+        demo_logo3 = PhotoImage(file="img/demo_logo.png")
+        width = 70
+        height = 70
+        demo_logo3 = demo_logo3.subsample(demo_logo3.width() // width, demo_logo3.height() // height)
+        btn_demo3=Button(second_frame,image=demo_logo3,text='demo3',font=('Georgia',14),bg="#A3842C",fg='black',
+                            cursor='hand2',height=110,width=230,compound="top")
+        btn_demo3.image = demo_logo3
+        btn_demo3.grid(row=4,column=2,padx=10,pady=10)
+
+        demo_logo4 = PhotoImage(file="img/demo_logo.png")
+        width = 70
+        height = 70
+        demo_logo4 = demo_logo4.subsample(demo_logo4.width() // width, demo_logo4.height() // height)
+        btn_demo4=Button(second_frame,image=demo_logo4,text='demo4',font=('Georgia',14),bg="#A3842C",fg='black',
+                            cursor='hand2',height=110,width=230,compound="top")
+        btn_demo4.image = demo_logo4
+        btn_demo4.grid(row=4,column=3,padx=10,pady=10)
         
         self.update_content()
 #-----------------------------------------------------------------------------------------------------------------
